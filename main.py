1b64a43a7070dbf04778700fcfd138bba8117345
govind-for-data <govind4data@gmail.com>
2026-01-31 15:50:06 +0530

    Code cleaup

--- main.py
+++ main.py
@@ -3,65 +3,7 @@ done_tasks = []
 while True:
     user_actions = input("type add, show, edit, complete or exit: ")
     user_actions = user_actions.strip()
-
-    # match user_actions:
-    #     case "add":
-    #         task = input("Enter the task to add: ") + "\n"
-    #         # file = open('list.txt','r') 
-    #         # todos = file.readlines()
-    #         # file.close()
-    #         ## this way we open a external file(list.txt). 'r' is read only permission.
-    #         ## there is another way of doing this which is more efficient way to handle an external file.
-    #         with open('list.txt','r') as file:
-    #             todos = file.readlines()
-    #         todos.append(task)
-    #         ## open the file with the 'w' write permission.            
-    #         # file = open('list.txt','w')  
-    #         # file.writelines(todos)
-    #         # file.close()
-    #         with open('list.txt','w') as file:
-    #             file.writelines(todos)
-
-    #     case "show":
-    #         # file = open('list.txt', 'r')
-    #         # todos = file.readlines()
-    #         # file.close()
-    #         with open('list.txt','r') as file:
-    #             todos = file.readlines()
-    #         ## list comprihension : new_todos = [item.strip('\n') for i in todos]            
-    #         for i, item in enumerate(todos):
-    #             item = item.strip('\n')
-    #             print(f"{i+1}: {item.title()}")
-
-    #     case "edit":
-    #         number = int(input("Enter the number: "))
-    #         index = number - 1
-    #         new_task = input("Enter new task: ") + '\n'
-
-    #         with open('list.txt','r') as file:
-    #             todos = file.readlines()
-    #         todos[index]=new_task
-
-    #         with open('list.txt','w') as file:
-    #             file.writelines(todos)
-
-    #     case "complete":
-    #         number = int(input("Enter the task completed: "))
-    #         index = number - 1
-            
-    #         with open('list.txt','r') as file:
-    #             todos = file.readlines()
-            
-    #         done = todos.pop(index)
-    #         done_tasks.append(done)
-
-    #         with open('list.txt','w') as file:
-    #             file.writelines(todos)
-
-    #     case "exit":
-    #         break
-##### Better and optimized version of the code
-    
+##### Better and optimized version of the code    
     if user_actions.startswith("add"):
         task = user_actions[4:] + "\n"
 
