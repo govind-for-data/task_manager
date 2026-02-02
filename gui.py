03b06d190aeb9e65d66d32aedc6d0605d2e5950d
govind-for-data <govind4data@gmail.com>
2026-02-02 22:35:45 +0530

    Interactive add button functonality added

--- gui.py
+++ gui.py
@@ -3,9 +3,22 @@ import FreeSimpleGUI as sg
 import tkinter as tk
 
 lable = sg.Text("Type in a To-do")
-input_box = sg.InputText(tooltip="Enter to-do")
+input_box = sg.InputText(tooltip="Enter to-do",key='task')
 add_button = sg.Button("Add")
 
 window = sg.Window('My To-do List', layout=[[lable],[input_box,add_button]])
-window.read()
+
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todo_list = functions.read_file()
+            new_task = values['task'] + "\n"
+            todo_list.append(new_task)
+            functions.write_file(todo_list)
+        case sg.WINDOW_CLOSED:
+            break
+
+
+
 window.close()
