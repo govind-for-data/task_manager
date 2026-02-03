26e24ea33ed7c12e172afea4a965b615e75fe543
govind-for-data <govind4data@gmail.com>
2026-02-03 06:13:35 +0530

    Functunality of edit button added

--- gui.py
+++ gui.py
@@ -5,20 +5,37 @@ import tkinter as tk
 lable = sg.Text("Type in a To-do")
 input_box = sg.InputText(tooltip="Enter to-do",key='task')
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.read_file(),enable_events=True,
+                      size=(45,10),key='task_list')
+edit_button = sg.Button("Edit")
 
-window = sg.Window('My To-do List', layout=[[lable],[input_box,add_button]])
+window = sg.Window('My To-do List', layout=[[lable],[input_box,add_button],
+                                            [list_box,edit_button]])
 
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
     match event:
         case "Add":
             todo_list = functions.read_file()
             new_task = values['task'] + "\n"
             todo_list.append(new_task)
             functions.write_file(todo_list)
-        case sg.WINDOW_CLOSED:
+            window['task_list'].update(values=todo_list)
+        case "Edit":
+            task_to_edit = values['task_list'][0]
+            new_task = values['task'] + "\n"
+
+            todo_list = functions.read_file()
+            index = todo_list.index(task_to_edit)
+            todo_list[index] = new_task
+            functions.write_file(todo_list)
+            window['task_list'].update(values=todo_list)
+        case "task_list":
+            window['task'].update(value=values['task_list'][0])
+
+        case sg.WIN_CLOSED:
             break
-
-
 
 window.close()
