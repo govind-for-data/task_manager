ee6fbd84f8389c0f5f9431e6c7fa54a37edc38ee
govind-for-data <govind4data@gmail.com>
2026-02-03 12:53:03 +0530

    Changes in the task listing with a timestamp

--- gui.py
+++ gui.py
@@ -1,16 +1,21 @@
 import functions
 import FreeSimpleGUI as sg
 import tkinter as tk
+from datetime import datetime
 
 lable = sg.Text("Type in a To-do")
-input_box = sg.InputText(tooltip="Enter to-do",key='task')
+input_box = sg.InputText(tooltip="Enter to-do", key='task')
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values=functions.read_file(),enable_events=True,
-                      size=(45,10),key='task_list')
+list_box = sg.Listbox(values=functions.read_file(), enable_events=True,
+                      size=(45, 10), key='task_list')
 edit_button = sg.Button("Edit")
 
-window = sg.Window('My To-do List', layout=[[lable],[input_box,add_button],
-                                            [list_box,edit_button]])
+window = sg.Window('My To-do List', layout=[[lable], [input_box, add_button],
+                                            [list_box, edit_button]])
+
+def with_timestamp(task: str) -> str:
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M')
+    return f"{timestamp} - {task}\n"
 
 while True:
     event, values = window.read()
@@ -19,13 +24,15 @@ while True:
     match event:
         case "Add":
             todo_list = functions.read_file()
-            new_task = values['task'] + "\n"
+            new_task = with_timestamp(values['task'])
             todo_list.append(new_task)
             functions.write_file(todo_list)
             window['task_list'].update(values=todo_list)
         case "Edit":
+            if not values['task_list']:
+                continue
             task_to_edit = values['task_list'][0]
-            new_task = values['task'] + "\n"
+            new_task = with_timestamp(values['task'])
 
             todo_list = functions.read_file()
             index = todo_list.index(task_to_edit)
@@ -33,7 +40,12 @@ while True:
             functions.write_file(todo_list)
             window['task_list'].update(values=todo_list)
         case "task_list":
-            window['task'].update(value=values['task_list'][0])
+            if not values['task_list']:
+                continue
+            selected = values['task_list'][0].rstrip('\n')
+            if ' - ' in selected:
+                selected = selected.split(' - ', 1)[1]
+            window['task'].update(value=selected)
 
         case sg.WIN_CLOSED:
             break
